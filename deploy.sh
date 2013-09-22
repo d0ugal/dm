99614d38d1bcef80a39c9e3d25ae3eee1809c5e0
Dougal Matthews <dougal85@gmail.com>
2013-09-22 19:19:18 +0100

    Tweak the deploy process.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 ./build.sh;
-
+git push origin master;
 mv .gitignore .gitignore.bk;
 mv .gitignore_deploy .gitignore;
-heroku push --app dm-com;
+heroku push --app dm-com && heroku ps:scale web=0 && heroku ps:scale web=1;
 mv .gitignore .gitignore_deploy;
 mv .gitignore.bk .gitignore;
-heroku ps:scale web=0 && heroku ps:scale web=1;
