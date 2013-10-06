55ec0a46e31e26283103c8a8ecb958f8714d528a
Dougal Matthews <dougal85@gmail.com>
2013-10-06 17:43:08 +0100

    Added a post deploy check.

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ mv .gitignore_deploy .gitignore;
 heroku push --app dm-com && heroku ps:scale web=0 && heroku ps:scale web=1;
 mv .gitignore .gitignore_deploy;
 mv .gitignore.bk .gitignore;
+curl -I http://dougalmatthews.com;
