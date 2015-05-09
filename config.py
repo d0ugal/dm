4dd23bbded111387378994421d5e909e59a11d11
Dougal Matthews <dougal@redhat.com>
2015-05-09 15:17:46 +0100

    Add a CNAME

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ TIMEZONE = "Europe/London"
 
 SITENAME = "Dougal Matthews"
 
-DEFAULT_CATEGORY = 'Uncategorized'
+DEFAULT_CATEGORY = 'Uncategorised'
 
 TWITTER_USERNAME = 'd0ugal'
 
 PDF_GENERATOR = False
 REVERSE_CATEGORY_ORDER = True
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 100
 
 FEED_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
@@ -19,8 +19,6 @@ SOCIAL = (('twitter', 'http://twitter.com/d0ugal'),
 SITEURL = FEED_DOMAIN = "http://dougalmatthews.com"
 
 THEME = 'dm'
-
-DEFAULT_PAGINATION = 4
 
 ARTICLE_URL = '{date:%Y}/{date:%b}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
@@ -42,3 +40,6 @@ ARCHIVES_SAVE_AS = 'archives/index.html'
 DIRECT_TEMPLATES = ('index', 'tags', 'categories', 'archives', 'notes')
 
 JINJA_EXTENSIONS = ('jinja2.ext.loopcontrols', )
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
