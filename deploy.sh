c9180efbe90ea08f47c4af3299e63844bdb5d29d
Dougal Matthews <dougal@redhat.com>
2015-05-09 14:50:57 +0100

    Remove cruft. Why did I ever do this? I don't understand.

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,5 @@
 
 ./build.sh;
 
-git push origin master;
-mv .gitignore .gitignore.bk;
-mv .gitignore_deploy .gitignore;
-curl -I http://www.dougalmatthews.com;
-heroku push --app dm-com;
-mv .gitignore .gitignore_deploy;
-mv .gitignore.bk .gitignore;
-heroku logs -n 5;
-curl -I http://www.dougalmatthews.com;
-heroku logs -n 5;
+ghp-import output;
+git push origin ghp-import;
